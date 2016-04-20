dc5cb9461c4aeb071315d57de5a851508b392c05
T-Valley SungHyun Park <puresprout@sk.com>
2016-04-20 21:29:12 +0900

    initial commit

---
+++ src/main/java/com/purestation/signpostplus/OkHttpOAuthConsumerUsingBasicAuthAtHttps.java
@@ -0,0 +1,45 @@
+package com.purestation.signpostplus;
+
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
+import oauth.signpost.http.HttpRequest;
+import okhttp3.Request;
+import org.apache.commons.codec.binary.Base64;
+import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
+
+/**
+ * {@code OkHttpOAuthConsumerUsingBasicAuthAtHttps} is a {@link oauth.signpost.OAuthConsumer} implementation capable of handling OkHttp
+ * {@link Request}s. If the request is https, this uses basic authorization.
+ */
+public class OkHttpOAuthConsumerUsingBasicAuthAtHttps extends OkHttpOAuthConsumer {
+
+    private String encodedAuthStr;
+
+    /**
+     * Constructs a new {@code OkHttpOAuthConsumerUsingBasicAuthAtHttps}.
+     * @param consumerKey the consumer key.
+     * @param consumerSecret the consumer secret.
+     */
+    public OkHttpOAuthConsumerUsingBasicAuthAtHttps(String consumerKey, String consumerSecret) {
+        super(consumerKey, consumerSecret);
+
+        String authStr = getConsumerKey() + ":" + getConsumerSecret();
+        encodedAuthStr = new String(Base64.encodeBase64(authStr.getBytes()));
+    }
+
+    public synchronized HttpRequest sign(Object request) throws OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException {
+        return this.sign(this.wrap(request));
+    }
+
+    public synchronized HttpRequest sign(HttpRequest request) throws OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException {
+        if (request.getRequestUrl().startsWith("https://")) {
+            request.setHeader("Authorization", "Basic " + encodedAuthStr);
+
+            return request;
+        } else {
+            return super.sign(request);
+        }
+    }
+
+}
